1e20c850c68f2de3c9017a326ea4ce899372bf83
Tobias Jülg <tobias.juelg@utn.de>
2025-04-25 22:58:02 +0200

    initial commit

---
+++ pretix_stustapay_placeholders/apps.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+from . import __version__
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(PluginConfig):
+    default = True
+    name = "pretix_stustapay_placeholders"
+    verbose_name = "Pretix Placeholder Plugin for StuStaPay"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Pretix Placeholder Plugin for StuStaPay")
+        author = "Tobias Jülg"
+        description = gettext_lazy("Pretix plugin for email placeholder extensions in StuStaPay")
+        visible = True
+        version = __version__
+        category = "INTEGRATION"
+        compatibility = "pretix>=2.7.0"
+        settings_links = []
+        navigation_links = []
+
+    def ready(self):
+        from . import signals  # NOQA
